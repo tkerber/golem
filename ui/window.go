af4a2b79d4c91ab36ea6b7cd4f10fd221c42b086
Thomas Kerber <t.kerber@online.de>
2014-12-27 20:24:25 +0100

    Fix 'switching' from a tab to the same one.

--- ui/window.go
+++ ui/window.go
@@ -142,10 +142,10 @@ func (w *Window) ReplaceWebView(wv *webkit.WebView) {
 //
 // MUST ONLY BE INVOKED THROUGH GlibMainContextInvoke!
 func (w *Window) replaceWebView(wv *webkit.WebView) {
-	w.webViewBox.PackStart(wv, true, true, 0)
+	w.webViewBox.Remove(w.WebView)
 	w.WebView.Hide()
 	wv.Show()
-	w.webViewBox.Remove(w.WebView)
+	w.webViewBox.PackStart(wv, true, true, 0)
 	w.WebView = wv
 }
 
