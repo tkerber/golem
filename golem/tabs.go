99bf2e532c59e121cdabe333429fa8080765f12b
Thomas Kerber <t.kerber@online.de>
2015-01-07 20:16:29 +0100

    Force major operations to run in glib main context.

--- golem/tabs.go
+++ golem/tabs.go
@@ -3,6 +3,7 @@ package golem
 import (
 	"fmt"
 
+	"github.com/tkerber/golem/golem/ui"
 	"github.com/tkerber/golem/gtk"
 	"github.com/tkerber/golem/webkit"
 )
@@ -10,115 +11,157 @@ import (
 // NewTabs opens several new tabs to the specified URIs.
 //
 // If the URI is blank, the new tab page is used instead.
+//
+// NewTabs is a glib atomic operation, i.e. it is executed in glibs main
+// context.
 func (w *Window) NewTabs(uris ...string) ([]*webView, error) {
-	wvs := make([]*webView, len(uris))
-	wvs, err := w.newTabsWithWebViews(wvs...)
-	if err != nil {
-		return nil, err
-	}
-	for i, wv := range wvs {
-		if uris[i] == "" {
-			wv.LoadURI(w.parent.newTabPage)
-		} else {
-			wv.LoadURI(uris[i])
+	var wvs []*webView
+	var err error
+	gtk.GlibMainContextInvoke(func() {
+		wvs := make([]*webView, len(uris))
+		wvs, err = w.newTabsWithWebViews(wvs...)
+		if err != nil {
+			return
 		}
-	}
-	return wvs, nil
+		for i, wv := range wvs {
+			if uris[i] == "" {
+				wv.LoadURI(w.parent.newTabPage)
+			} else {
+				wv.LoadURI(uris[i])
+			}
+		}
+	})
+	return wvs, err
 }
 
-// newTabWithRequests opens a new tab and loads a specified uri request into
+// newTabWithRequest opens a new tab and loads a specified uri request into
 // it.
+//
+// newTabWithRequest is a glib atomic operation, i.e. it is executed in glibs
+// main context.
 func (w *Window) newTabWithRequest(req *webkit.URIRequest) (*webView, error) {
-	wvs, err := w.newTabsWithWebViews(nil)
-	if err != nil {
-		return nil, err
-	}
-	wvs[0].LoadRequest(req)
-	return wvs[0], nil
+	var wv *webView
+	var err error
+	gtk.GlibMainContextInvoke(func() {
+		var wvs []*webView
+		wvs, err = w.newTabsWithWebViews(nil)
+		if err != nil {
+			return
+		}
+		wvs[0].LoadRequest(req)
+		wv = wvs[0]
+	})
+	return wv, err
 }
 
 // newTabsWithWebViews creates tabs for several web views and attaches them
 // after the current tab.
 //
 // If nil is supplied as a web view, a new web view is created.
+//
+// newTabsWithWebViews is a glib atomic operation, i.e. it is executed in
+// glibs main context.
 func (w *Window) newTabsWithWebViews(wvs ...*webView) ([]*webView, error) {
-	for i, wv := range wvs {
-		if wv == nil {
-			wv, err := w.newWebView(w.getWebView().settings)
-			if err != nil {
-				return nil, err
+	var err error
+	gtk.GlibMainContextInvoke(func() {
+		for i, wv := range wvs {
+			if wv == nil {
+				var wv *webView
+				wv, err = w.newWebView(w.getWebView().settings)
+				if err != nil {
+					return
+				}
+				wvs[i] = wv
+			} else {
+				wv.window = w
 			}
-			wvs[i] = wv
-		} else {
-			wv.window = w
 		}
-	}
-	tabs, err := w.Window.TabBar.AddTabs(
-		w.currentWebView+1,
-		w.currentWebView+1+len(wvs))
-	if err != nil {
-		return nil, err
-	}
-	for i := 0; i < len(wvs); i++ {
-		wvs[i].tabUI = tabs[i]
-		wvs[i].tabUI.SetTitle(wvs[i].GetTitle())
-	}
-	w.wMutex.Lock()
-	defer w.wMutex.Unlock()
-	// At the new tab directly after the current one.
-	newWebViews := make([]*webView, len(w.webViews)+len(wvs))
-	copy(newWebViews[:w.currentWebView+1], w.webViews[:w.currentWebView+1])
-	copy(newWebViews[w.currentWebView+1:w.currentWebView+1+len(wvs)], wvs)
-	copy(newWebViews[w.currentWebView+1+len(wvs):],
-		w.webViews[w.currentWebView+1:])
-	w.webViews = newWebViews
-	for _, wv := range wvs {
-		w.Window.AttachWebView(wv)
-	}
-	w.Window.TabCount = len(w.webViews)
-	go w.UpdateLocation()
+		var tabs []*ui.TabBarTab
+		tabs, err = w.Window.TabBar.AddTabs(
+			w.currentWebView+1,
+			w.currentWebView+1+len(wvs))
+		if err != nil {
+			return
+		}
+		for i := 0; i < len(wvs); i++ {
+			wvs[i].tabUI = tabs[i]
+			wvs[i].tabUI.SetTitle(wvs[i].GetTitle())
+		}
+		w.wMutex.Lock()
+		defer w.wMutex.Unlock()
+		// At the new tab directly after the current one.
+		newWebViews := make([]*webView, len(w.webViews)+len(wvs))
+		copy(newWebViews[:w.currentWebView+1], w.webViews[:w.currentWebView+1])
+		copy(newWebViews[w.currentWebView+1:w.currentWebView+1+len(wvs)], wvs)
+		copy(newWebViews[w.currentWebView+1+len(wvs):],
+			w.webViews[w.currentWebView+1:])
+		w.webViews = newWebViews
+		for _, wv := range wvs {
+			w.Window.AttachWebView(wv)
+		}
+		w.Window.TabCount = len(w.webViews)
+		go w.UpdateLocation()
+	})
 	// Note that we do *not* switch tabs here.
-	return wvs, nil
+	return wvs, err
 }
 
 // tabNext goes to the next tab.
+//
+// tabNext is a glib atomic operation, that is, it is executed in glibs main
+// context.
 func (w *Window) tabNext() {
 	w.tabGo((w.currentWebView + 1) % len(w.webViews))
 }
 
 // tabPrev goes to the previous tab.
+//
+// tabPrev is a glib atomic operation, that is, it is executed in glibs main
+// context.
 func (w *Window) tabPrev() {
 	w.tabGo((w.currentWebView + len(w.webViews) - 1) % len(w.webViews))
 }
 
 // tabGo goes to a specified tab.
+//
+// tabGo is a glib atomic operation, that is, it is executed in glibs main
+// context.
 func (w *Window) tabGo(index int) error {
-	if index >= len(w.webViews) || index < 0 {
-		return fmt.Errorf("Illegal tab index: %v", index)
-	}
-	w.wMutex.Lock()
-	defer w.wMutex.Unlock()
-	w.currentWebView = index
-	w.Window.TabNumber = index + 1
-	wv := w.getWebView()
-	w.reconnectWebViewSignals()
-	w.SwitchToWebView(wv)
-	w.Window.TabBar.FocusTab(index)
-	go w.UpdateLocation()
-	return nil
+	var err error
+	gtk.GlibMainContextInvoke(func() {
+		if index >= len(w.webViews) || index < 0 {
+			err = fmt.Errorf("Illegal tab index: %v", index)
+			return
+		}
+		w.wMutex.Lock()
+		defer w.wMutex.Unlock()
+		w.currentWebView = index
+		w.Window.TabNumber = index + 1
+		wv := w.getWebView()
+		w.reconnectWebViewSignals()
+		w.SwitchToWebView(wv)
+		w.Window.TabBar.FocusTab(index)
+		go w.UpdateLocation()
+	})
+	return err
 }
 
 // tabsClose closes the tabs from index i to j (slice indexes)
+//
+// tabsClose is a glib atomic operation, that is, it is executed in glibs main
+// context.
 func (w *Window) tabsClose(i, j int, cut bool) {
 	if len(w.webViews) == j-i {
-		if cut {
-			for _, wv := range w.webViews {
-				wv.detach()
+		gtk.GlibMainContextInvoke(func() {
+			if cut {
+				for _, wv := range w.webViews {
+					wv.detach()
+				}
+				w.parent.cutWebViews(w.webViews)
+				w.webViews = make([]*webView, 0)
 			}
-			w.parent.cutWebViews(w.webViews)
-			w.webViews = make([]*webView, 0)
-		}
-		w.Window.Close()
+			w.Window.Close()
+		})
 		return
 	}
 	w.wMutex.Lock()
