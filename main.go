35e94d5d8fbac1ded59866478d17d0090f8f336a
Thomas Kerber <t.kerber@online.de>
2015-02-13 22:22:09 +0000

    Started rewriting to sockets.

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
+	"net/rpc/jsonrpc"
 	"os"
 	"regexp"
 	"runtime"
 
 	"github.com/conformal/gotk3/gtk"
-	"github.com/guelfey/go.dbus"
-	"github.com/guelfey/go.dbus/introspect"
 	"github.com/mattn/go-shellwords"
 	"github.com/tkerber/golem/golem"
 )
@@ -50,101 +50,87 @@ func main() {
 	}
 	args := flag.Args()
 
-	// Try to acquire the golem bus
-	sBus, err := dbus.SessionBus()
+	acquireSocket(
+		profile,
+		func(l net.Listener) { socketAcquired(l, profile, args) },
+		func(c net.Conn) { socketFound(c, args) })
+}
+
+// socketAcquired is called when golem obtains ownership of the socket, and
+// starts up the browser. Note that the Listener is closed outwith this method.
+func socketAcquired(l net.Listener, profile string, args []string) {
+	gtk.Init(&args)
+	g, err := golem.New((*golem.Session)(l), profile)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to acquire session bus: %v", err))
+		panic(fmt.Sprintf("Error during golem initialization: %v", err))
 	}
-	repl, err := sBus.RequestName(
-		fmt.Sprintf(golem.DBusName, profile),
-		dbus.NameFlagDoNotQueue)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to ascertain status of Golem's bus name."))
-	}
-	switch repl {
-	// If we get it, this is the new golem. Hurrah!
-	case dbus.RequestNameReplyPrimaryOwner:
-		gtk.Init(&args)
-		g, err := golem.New((*golem.Session)(sBus), profile)
+	defer g.WebkitCleanup()
+	// TODO: add json-rpc object to serve the listener at l.
+
+	// All arguments are taken as "open" commands for one tab each.
+	// They will load in reverse order; i.e. with the last as the top
+	// tab, to be consistent with golem's load order.
+	uris := make([]string, len(args))
+	for i, arg := range args {
+		// we try to split it into parts to allow searches to be passed
+		// via command line. If this fails, we ignore the error and just
+		// pass the whole string instead.
+		parts, err := shellwords.Parse(arg)
 		if err != nil {
-			panic(fmt.Sprintf("Error during golem initialization: %v", err))
+			parts = []string{arg}
 		}
-		defer g.WebkitCleanup()
-		sBus.Export(
-			g.CreateDBusWrapper(),
-			golem.DBusPath,
-			golem.DBusInterface)
-		sBus.Export(
-			introspect.Introspectable(golem.DBusIntrospection),
-			golem.DBusPath,
-			"org.freedesktop.DBus.Introspectable")
-		// All arguments are taken as "open" commands for one tab each.
-		// They will load in reverse order; i.e. with the last as the top
-		// tab, to be consistent with golem's load order.
-		uris := make([]string, len(args))
-		for i, arg := range args {
-			// we try to split it into parts to allow searches to be passed
-			// via command line. If this fails, we ignore the error and just
-			// pass the whole string instead.
-			parts, err := shellwords.Parse(arg)
-			if err != nil {
-				parts = []string{arg}
-			}
-			uris[i] = g.OpenURI(parts)
+		uris[i] = g.OpenURI(parts)
+	}
+	if len(uris) == 0 {
+		_, err := g.NewWindow("")
+		if err != nil {
+			golem.Errlog.Printf("Failed to open window: %v", err)
+			exitCode = 1
+			return
 		}
-		if len(uris) == 0 {
-			_, err := g.NewWindow("")
+	} else {
+		// Open the last tab in the new window, then open all others in
+		// order in a new tab.
+		win, err := g.NewWindow(uris[0])
+		if err != nil {
+			golem.Errlog.Printf("Failed to open window: %v", err)
+			exitCode = 1
+			return
+		}
+		if len(uris) > 1 {
+			_, err = win.NewTabs(uris[1:]...)
 			if err != nil {
-				golem.Errlog.Printf("Failed to open window: %v", err)
-				exitCode = 1
-				return
-			}
-		} else {
-			// Open the last tab in the new window, then open all others in
-			// order in a new tab.
-			win, err := g.NewWindow(uris[0])
-			if err != nil {
-				golem.Errlog.Printf("Failed to open window: %v", err)
-				exitCode = 1
-				return
-			}
-			if len(uris) > 1 {
-				_, err = win.NewTabs(uris[1:]...)
-				if err != nil {
-					golem.Errlog.Printf("Failed to open tabs: %v", err)
-				}
+				golem.Errlog.Printf("Failed to open tabs: %v", err)
 			}
 		}
-		// This doesn't need to run in a goroutine, but as the gtk main
-		// loop can be stopped and restarted in a goroutine, this makes
-		// more sense.
-		go gtk.Main()
-		handleSignals(g)
-		<-g.Quit
-		sBus.ReleaseName(golem.DBusName)
-	// If not, we attach to the existing one.
-	default:
-		o := sBus.Object(
-			fmt.Sprintf(golem.DBusName, profile),
-			golem.DBusPath)
-		// If there are no uris, instead create a new window.
-		if len(args) == 0 {
-			call := o.Call(
-				golem.DBusInterface+".NewWindow",
-				0)
-			if call.Err != nil {
-				golem.Errlog.Printf("Failed to open window: %v", call.Err)
-				exitCode = 1
-				return
-			}
-		} else {
-			call := o.Call(
-				golem.DBusInterface+".NewTabs",
-				0,
-				args)
-			if call.Err != nil {
-				golem.Errlog.Printf("Failed to open tabs: %v", call.Err)
-			}
+	}
+	// This doesn't need to run in a goroutine, but as the gtk main
+	// loop can be stopped and restarted in a goroutine, this makes
+	// more sense.
+	go gtk.Main()
+	handleSignals(g)
+	<-g.Quit
+}
+
+// socketFound is executed when a socket occupied by a running golem instance
+// if found. It communicates with the running golem. (Note that the connection
+// if closed outwith this function)
+func socketFound(c net.Conn, args []string) {
+	rpc := jsonrpc.NewClient(c)
+	// If there are no uris, instead create a new window.
+	if len(args) == 0 {
+		err := rpc.Call("Golem.NewWindow", nil, nil)
+		if err != nil {
+			golem.Errlog.Printf("Failed to open window: %v", err)
+			exitCode = 1
+			return
+		}
+	} else {
+		err := rpc.Call("Golem.NewTabs", args, nil)
+		if err != nil {
+			golem.Errlog.Printf("Failed to open tabs: %v", err)
+			exitCode = 1
+			return
 		}
 	}
 }
